b437b9fb5ca0ff7536c97f15e7988e934d81a0a7
rahidulislampritom <rahidulislampritom@gmail.com>
2025-02-18 01:07:44 +0600

    header section design completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css' 
+import Header from './components/Header/Header'
+
+function App() {
+
+
+  return (
+   <div className='md:max-w-7xl mx-auto px-3 '>
+    <Header></Header>
+   </div>
+  )
+}
+
+export default App
