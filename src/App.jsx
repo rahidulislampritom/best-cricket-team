51400c84c8247ac3e1946b6b9e636f7299ef7316
rahidulislampritom <rahidulislampritom@gmail.com>
2025-02-18 11:57:41 +0600

    click free claim credit button to add coin

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css' 
+import { useState } from 'react'
+import './App.css'
 import Header from './components/Header/Header'
 
 function App() {
 
+  const [freeCredit, setFreeCredit] = useState(500000);
+
+  const handleFreeCredit = () => {
+    // console.log('hi');
+    const newFreeCredit = freeCredit + 100000;
+    setFreeCredit(newFreeCredit);
+    console.log(freeCredit);
+  }
 
   return (
-   <div className='md:max-w-7xl mx-auto px-3 '>
-    <Header></Header>
-   </div>
+    <div className='md:max-w-7xl mx-auto px-3 '>
+      <Header handleFreeCredit={handleFreeCredit} freeCredit={freeCredit}></Header>
+    </div>
   )
 }
 
